b4b1c29670d0bc8dedf6688bdd660f4c349795c2
Rahul Yadav <way2rahuly@gmail.com>
2019-10-03 02:02:29 +0530

    bash script fixed

--- runall.sh
+++ runall.sh
@@ -1,20 +1,29 @@
 ./compile.sh
-if [ $1 == "m" ]; then
-	./getGraph.out
-else
-	if [ $# -eq 0 ]; then
-		echo "nodes_phone edges_phone nodes_email edges_email isSubset"
-		read n_phone
-		read e_phone
-		read n_email
-		read e_email
-		read isSubset
-		python ./Code/generate.py n_phone e_phone n_email e_email isSubset > test.graphs
-	else	
-		python ./Code/generate.py $1 $2 $3 $4 $5 > test.graphs
+if [ $# -eq 1 ]; then
+	if [ $1 == '-m' ]; then
+		./getGraph.out
 	fi
+elif [ $# -eq 2 ];then
+	if [ $1 == '-f' ]; then
+		echo $2
+	fi
+elif [ $# -eq 0 ]; then
+	read -p $'5 parameters required: [nodes_phone edges_phone nodes_email edges_email isSubset]\n' n_phone e_phone n_email e_email isSubset
+	# read e_phone
+	# read n_email
+	# read e_email
+	# read isSubset
+	# echo "parameters read successfully: $n_phone $e_phone $n_email $e_email $isSubset"
+	python ./Code/generate.py $n_phone $e_phone $n_email $e_email $isSubset > test.graphs
+	echo "test graph generated"
+elif [ $# -eq 5 ]; then	
+	python ./Code/generate.py $1 $2 $3 $4 $5 > test.graphs
+else
+	echo "Recheck arguments and retry"
 fi
 ./run1.sh
 ./minisat/core/minisat test.satinput test.satoutput
 ./run2.sh
-less test.mapping
+echo "============================[ Mapping begins here ]============================"
+less -FX test.mapping
+echo "=============================[ Mapping ends here ]============================="
