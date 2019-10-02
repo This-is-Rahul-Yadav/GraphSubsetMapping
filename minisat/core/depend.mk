bfcf48c23d62fb8e553f7551487ea969884f0337
Rahul Yadav <way2rahuly@gmail.com>
2019-10-02 23:58:20 +0530

    syntax and link errors for mac fixed

---
+++ minisat/core/depend.mk
@@ -0,0 +1,29 @@
+/Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/core/Main.o /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/core/Main.or /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/core/Main.od /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/core/Main.op: \
+  /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/core/Main.cc \
+  /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/utils/System.h \
+  /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/mtl/IntTypes.h \
+  /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/utils/ParseUtils.h \
+  /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/utils/Options.h \
+  /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/mtl/Vec.h \
+  /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/mtl/XAlloc.h \
+  /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/core/Dimacs.h \
+  /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/core/SolverTypes.h \
+  /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/mtl/Alg.h \
+  /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/mtl/Map.h \
+  /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/mtl/Alloc.h \
+  /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/core/Solver.h \
+  /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/mtl/Heap.h
+/Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/core/Solver.o /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/core/Solver.or /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/core/Solver.od /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/core/Solver.op: \
+  /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/core/Solver.cc \
+  /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/mtl/Sort.h \
+  /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/mtl/Vec.h \
+  /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/mtl/IntTypes.h \
+  /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/mtl/XAlloc.h \
+  /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/core/Solver.h \
+  /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/mtl/Heap.h \
+  /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/mtl/Alg.h \
+  /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/utils/Options.h \
+  /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/utils/ParseUtils.h \
+  /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/core/SolverTypes.h \
+  /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/mtl/Map.h \
+  /Users/anilyadav/Rahul/IITD/Sem5/COL333/Assignment3/minisat/mtl/Alloc.h
